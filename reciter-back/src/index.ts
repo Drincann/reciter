c1f42d340c3a31cfbbbfd49842172a3f76009e48
Drincann <1019933576@qq.com>
2022-04-03 18:22:16 +0800

    feature: 为 koa 添加 body 解析

--- reciter-back/src/index.ts
+++ reciter-back/src/index.ts
@@ -2,17 +2,16 @@ import Koa from 'koa'
 import { config } from './config'
 import { router } from './route'
 import serve from 'koa-static'
+// body
+import bodyParser from 'koa-bodyparser'
 
 const app = new Koa()
 
 app
+  .use(bodyParser())
+
   .use(router.routes())
 
   .use(serve(config.staticRoot))
 
   .listen(config.port)
-
-
-
-
-
